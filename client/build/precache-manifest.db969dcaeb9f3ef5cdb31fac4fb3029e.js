5185444e6fc8ecc3eddc17341769d425e7d69958
Jeff <deutsch.jeff.a@gmail.com>
2020-04-25 20:42:09 -0400

    move front-end files to client folder and begin setup back-end server

---
+++ client/build/precache-manifest.db969dcaeb9f3ef5cdb31fac4fb3029e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a251b350fafe2199776bb427f37432cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "287766905ce3b793c21f",
+    "url": "/static/css/main.7bf728a1.chunk.css"
+  },
+  {
+    "revision": "aa72c5ab75c8ee2481d3",
+    "url": "/static/js/2.fbff5863.chunk.js"
+  },
+  {
+    "revision": "6985d75ffd76ce4f1484f26e7c7f1ad2",
+    "url": "/static/js/2.fbff5863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "287766905ce3b793c21f",
+    "url": "/static/js/main.64f0ee20.chunk.js"
+  },
+  {
+    "revision": "2d5632c0c498adbf8f2d",
+    "url": "/static/js/runtime-main.81fc5d41.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
